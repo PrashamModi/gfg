ff06b8f8392efcfcbba2d9b207e45cf294998b6b
PrashamJodd <109425729+PrashamModi@users.noreply.github.com>
2023-11-24 09:41:41 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pascal Triangle/pascal-triangle.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// #define ll long long
+class Solution{
+public:
+    int mod = 1e9 + 7;
+    vector<long long> nthRowOfPascalTriangle(int n) {
+        // code here
+        vector<long long> prev;
+        for(int i = 0; i < n; i++) {
+            vector<long long> curr(i + 1, 0);
+            for(int j = 0; j <= i; j++) {
+                if(j == 0 || j == i) curr[j] = 1;
+                else curr[j] = (prev[j - 1] + prev[j]) % mod;
+            }
+            prev = curr;
+        }
+        return prev;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+void printAns(vector<long long> &ans) {
+    for (auto &x : ans) {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.nthRowOfPascalTriangle(n);
+        printAns(ans);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
