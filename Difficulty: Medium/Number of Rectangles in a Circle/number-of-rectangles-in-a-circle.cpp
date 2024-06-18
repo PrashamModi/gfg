54b0b6ee84af77b077f0cf4c57f2ff09dfd8e695
PrashamJodd <109425729+PrashamModi@users.noreply.github.com>
2024-06-18 19:01:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Rectangles in a Circle/number-of-rectangles-in-a-circle.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int rectanglesInCircle(int r) {
+        // code here
+         int ans = 0;
+        for(int i = 1;i*i<=4*r*r - 1;i++){
+            ans += sqrt(r*r*4 - i*i);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        int ans = ob.rectanglesInCircle(n);
+        cout << ans << "\n";
+    }
+}
+// } Driver Code Ends
